54add87e6055c7a10691e1214aa61663de768563
herringg369 <herringg369@gmail.com>
2021-08-16 13:30:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-let movies = []
+let tenMovies = [{
+    title: 'Kung Fu Panda 2',
+    releaseDate: 2003
+}, {
+    title: 'Scott Pilgrim Vs. The World',
+    releaseDate: 2010
+}, {
+    title: 'The Social Network',
+    releaseDate: 2010
+}, {
+    title: 'A Silent Voice',
+    releaseDate: 2016
+}, {
+    title: 'Avengers Infinity War',
+    releaseDate: 2018
+}, {
+    title: 'The Emperors New Groove',
+    releaseDate: 2000
+}, {
+    title: 'Anomolisa',
+    releaseDate: 2015
+}, {
+    title: 'Spirited Away',
+    releaseDate: 2001
+}, {
+    title: 'John Wick',
+    releaseDate: 2014
+}, {
+    title: 'Mulan',
+    releaseDate: 1998
+}]
