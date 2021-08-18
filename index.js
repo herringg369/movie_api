2bbdc0a7e995cbe0e05c556aea4b4b40c91f4de6
herringg369 <herringg369@gmail.com>
2021-08-18 15:57:37 -0400

    updated the array to include the directors

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 let tenMovies = [{
     title: 'Kung Fu Panda 2',
-    releaseDate: 2003
+    releaseDate: 2003,
+    director: 'Jennefir Yuh Nelsion'
 }, {
     title: 'Scott Pilgrim Vs. The World',
-    releaseDate: 2010
+    releaseDate: 2010,
+    director: 'Edgar Wright'
 }, {
     title: 'The Social Network',
-    releaseDate: 2010
+    releaseDate: 2010,
+    director: 'David Fincher'
 }, {
     title: 'A Silent Voice',
-    releaseDate: 2016
+    releaseDate: 2016,
+    director: 'Naoko Yamada'
 }, {
     title: 'Avengers Infinity War',
-    releaseDate: 2018
+    releaseDate: 2018,
+    director: 'Joe Russo/Antho Russo'
 }, {
     title: 'The Emperors New Groove',
-    releaseDate: 2000
+    releaseDate: 2000,
+    director: 'Mark Dindal'
 }, {
     title: 'Anomolisa',
-    releaseDate: 2015
+    releaseDate: 2015,
+    director: 'Charlie Kaufman/Duke Johnson'
 }, {
     title: 'Spirited Away',
-    releaseDate: 2001
+    releaseDate: 2001,
+    director: 'Hayao Miyazaki'
 }, {
     title: 'John Wick',
-    releaseDate: 2014
+    releaseDate: 2014,
+    director: 'Chad Stahelski'
 }, {
     title: 'Mulan',
-    releaseDate: 1998
+    releaseDate: 1998,
+    director: 'Tony Bancroft/Barry Cook'
 }]
