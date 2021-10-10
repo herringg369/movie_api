22649e463c7fd5ffc12afef18985e75b286f6b66
herringg369 <herringg369@gmail.com>
2021-10-09 21:37:17 -0400

    trying to see if anything in auth.js is failing

--- auth.js
+++ auth.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken'),
 
 require('./passport'); // Your local passport file
 
+
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username, // This is the username you’re encoding in the JWT
@@ -17,20 +18,20 @@ let generateJWTToken = (user) => {
 /* POST login. */
 module.exports = (router) => {
   router.post('/login', (req, res) => {
-      passport.authenticate('local', { session: false }, (error, user, info) => {
-          if (error || !user) {
-              return res.status(400).json({
-                  message: 'Something is not right',
-                  user: user
-              });
-          }
-          req.login(user, { session: false }, (error) => {
-              if (error) {
-                  res.send(error);
-              }
-              let token = generateJWTToken(user.toJSON());
-              return res.json({ user: user, token: token });
-          });
-      })(req, res);
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
   });
 }
