c4c8fcd90713391aca6fda7c9e606ab0577975af
herringg369 <herringg369@gmail.com>
2021-10-26 19:36:00 -0400

    temporary removal of token authorization

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ let auth = require('./auth')(app)
 require('./passport.js')
 
 app.get('/movies',
- passport.authenticate('jwt', { session: false }),
+ // passport.authenticate('jwt', { session: false }),
   (req, res) => {
   Movies.find().then((movies) => {
     res.status(201).json(movies)
